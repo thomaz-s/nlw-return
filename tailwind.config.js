c410fdc885fe4b6cb907d22dcf2d1fb1dc969c0c
Thomaz <thomazsousa@outlook.com.br>
2022-07-22 10:32:29 -0300

    ultima aula, com alguns bugs

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: ["./src/**/*.{html,js,tsx}"],
+  theme: {
+	extend: {
+	  colors: {
+		brand: {
+		  	300: '#996DFF',
+			500: '#8257e6',
+		}
+	  },
+
+	  borderRadius: {
+		  md: '4px'
+	  }
+	},
+  },
+  plugins: [
+	require('tailwind-scrollbar'),
+	require('@tailwindcss/forms')
+  ],
+}
